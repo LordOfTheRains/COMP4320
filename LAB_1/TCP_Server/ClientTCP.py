ef10038b5a43166f9b090b88a9f5209d384f3432
bnr0010 <31320217+bnr0010@users.noreply.github.com>
2017-09-20 09:22:27 -0500

    Update ClientTCP.py

--- LAB_1/TCP_Server/ClientTCP.py
+++ LAB_1/TCP_Server/ClientTCP.py
@@ -1,24 +1,25 @@
+#!/usr/bin/python                                                               
+
 from socket import *
 import time
-import struct
 import sys
 
 class ClientTCP:
-#keep track of request number
+   #keep track of request number                                                
    requestID = 0
 
-#initialize client 
+   #initialize client                                                           
    def __init__(self, servername, portnumber):
-		self.servername = servername
-		self.portnumber = portnumber
-		self.tcpSocket = self.connectToServer((servername, portnumber))
+      self.servername = servername
+      self.portnumber = portnumber
+      self.tcpSocket = self.connectToServer((servername, portnumber))
 
-#connect to TCP server
+   #connect to TCP server                                                       
    def connectToServer(self, servAddr):
-		tcpSocket = socket(AF_INET, SOCK_STREAM)
-		tcpSocket.connect(servAddr)
+      tcpSocket = socket(AF_INET, SOCK_STREAM)
+      tcpSocket.connect(servAddr)
       
-		return tcpSocket
+      return tcpSocket
       
 #number of consonants in s
    def cLength(self, s):
@@ -60,29 +61,37 @@ class ClientTCP:
 
 #main
 #argv: client servername portnumber operation string
-   def main(argv):
-      client, servername, portnumber, operation, s = sys.argv
-      portnumber = int(portnumber)
-      operation = int(operation)
-      client = ClientTCP(servername, portnumber)
-      if operation == 5:
-         startTime = time.time()
-         print "Number of consonants in the string \"{}\".".format(s)
-         result = client.cLength(s)
-         endTime = time.time()
-      elif operation == 80:
-         startTime = time.time()
-         print "Disemvowel the string \"{}\".".format(s)
-         result = client.Disemvowel(s)
-         endTime = time.time()
-      elif operation == 10:
-         startTime = time.time()
-         print "Uppercase the string \"{}\".".format(s)
-         result = client.Uppercase(s)
-         endTime = time.time()
-      else: 
-         print "Invalid Operation"
-      print "\nRequestId: {}".format(result[1])
-      print "\nResponse:  {}".format(result[2])
-      print "\nRound trip time: {}s".format(endTime-startTime)
+if __name__ == "__main__":
+   if len(sys.argv) != 5:
+      print "usage: client servername portnumber operation string"       
+      sys.exit()
+   client = sys.argv[0]
+   servername = sys.argv[1]
+   portnumber = sys.argv[2]
+   operation = sys.argv [3]
+   s = sys.argv[4]
+   portnumber = int(portnumber)
+   operation = int(operation)
+   client = ClientTCP(servername, portnumber)
+   if operation == 5:
+      startTime = time.time()
+      print "Number of consonants in the string \"{}\".".format(s)
+      result = client.cLength(s)
+      endTime = time.time()
+   elif operation == 80:
+      startTime = time.time()
+      print "Disemvowel the string \"{}\".".format(s)
+      result = client.Disemvowel(s)
+      endTime = time.time()
+   elif operation == 10:
+      startTime = time.time()
+      print "Uppercase the string \"{}\".".format(s)
+      result = client.Uppercase(s)
+      endTime = time.time()
+   else:
+      print "Invalid Operation"
+      sys.exit()
+   print "\nRequestId: {}".format(result[1])
+   print "\nResponse:  {}".format(result[2])
+   print "\nRound trip time: {}s".format(endTime-startTime)
    sys.exit()
